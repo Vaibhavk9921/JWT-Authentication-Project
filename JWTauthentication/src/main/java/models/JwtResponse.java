2d3a7f1269fdcbccc69b4c5d42d98b10d4e76fb8
Vaibhav Jagannath Kadam <107555072+Vaibhavk9921@users.noreply.github.com>
2024-12-01 15:26:54 +0530

    JwtResponse.java

--- JWTauthentication/src/main/java/models/JwtResponse.java
+++ JWTauthentication/src/main/java/models/JwtResponse.java
@@ -1,27 +1,16 @@
 package models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString
 public class JwtResponse {
     private String jwtToken;
     private String username;
 
+    // Private constructor to enforce the use of the builder
     private JwtResponse(String jwtToken, String username) {
         this.jwtToken = jwtToken;
         this.username = username;
     }
 
+    // Getters for the fields
     public String getJwtToken() {
         return jwtToken;
     }
@@ -30,26 +19,28 @@ public class JwtResponse {
         return username;
     }
 
-    public static JwtResponseBuilder builder() {
-        return new JwtResponseBuilder();
-    }
-
-    public static class JwtResponseBuilder {
+    // Static Builder class
+    public static class Builder {
         private String jwtToken;
         private String username;
 
-        public JwtResponseBuilder jwtToken(String jwtToken) {
+        public Builder jwtToken(String jwtToken) {
             this.jwtToken = jwtToken;
-            return this;
+            return this; // Return the builder for method chaining
         }
 
-        public JwtResponseBuilder username(String username) {
+        public Builder username(String username) {
             this.username = username;
-            return this;
+            return this; // Return the builder for method chaining
         }
 
         public JwtResponse build() {
             return new JwtResponse(jwtToken, username);
         }
+    }
+
+    // Static factory method to initiate the builder
+    public static Builder builder() {
+        return new Builder();
     }
 }
