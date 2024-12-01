10c24e5fc3a9b985dffb6d9384a2ef48c5f32697
Vaibhav Jagannath Kadam <107555072+Vaibhavk9921@users.noreply.github.com>
2024-12-01 15:25:59 +0530

    HomeController.java

--- JWTauthentication/src/main/java/controller/HomeController.java
+++ JWTauthentication/src/main/java/controller/HomeController.java
@@ -3,6 +3,11 @@ package controller;
 import java.security.Principal;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +18,27 @@ import services.UserService;
 @RestController
 @RequestMapping("/home")
 public class HomeController {
-	private UserService userService;
+
+	private final UserService userService;
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	public HomeController(UserService userService) {
+		this.userService = userService;
+	}
 
 	// localhost:8080/home/user
-	@RequestMapping("/user")
+	@GetMapping("/user")
 	public List<User> getUser() {
-		System.out.println("Getting Users ");
+		logger.info("Fetching all users.");
 		return userService.getUsers();
 	}
-	 @GetMapping("/current-user")
-	    public String getLoggedInUser(Principal principal){
-	        return principal.getName();
-	    }
+
+	@GetMapping("/current-user")
+	public ResponseEntity<?> getLoggedInUser(Principal principal) {
+		if (principal == null) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User is not authenticated");
+		}
+		return ResponseEntity.ok(principal.getName());
+	}
 }
