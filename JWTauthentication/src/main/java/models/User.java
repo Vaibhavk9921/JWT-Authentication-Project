716ecc18efd7c3be3c0c70929e88fb434586a58b
Vaibhav Jagannath Kadam <107555072+Vaibhavk9921@users.noreply.github.com>
2024-12-01 15:27:26 +0530

    User.java

--- JWTauthentication/src/main/java/models/User.java
+++ JWTauthentication/src/main/java/models/User.java
@@ -12,10 +12,37 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class User {
-	public User(String string, String string2, String string3) {
+    public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	private String userId;
-	private String name;
-	private String email;
+    private String name;
+    private String email;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
 }
